e8854651f7cf47a0c877028042b6238171e490e5
Naufal Pratama <32818495+noizell@users.noreply.github.com>
2024-05-28 20:04:44 +0700

    Update get_files.php

--- Markers/get_files.php
+++ Markers/get_files.php
@@ -1,13 +1,15 @@
 <?php
+header('Content-Type: application/json');
 $directory = __DIR__;
+$files = array();
+
 if ($handle = opendir($directory)) {
-    $files = [];
     while (false !== ($file = readdir($handle))) {
         if ($file != "." && $file != "..") {
             $files[] = $file;
         }
     }
     closedir($handle);
-    echo json_encode($files);
 }
+echo json_encode($files);
 ?>
